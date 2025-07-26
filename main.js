f3920fd416932d55fe16f488195238e9324ecd63
Toskopadoo <numseilucasgabriel@gmail.com>
2025-07-26 09:56:29 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 fetch('/api/conteudo')
-  .then(r => r.text())
-  .then(texto => {
-    document.getElementById('app').innerHTML = texto;
+  .then(res => res.text())
+  .then(html => {
+    document.open();
+    document.write(html);
+    document.close();
+  })
+  .catch(err => {
+    document.getElementById('app').innerHTML = 'Erro ao carregar o site.';
+    console.error(err);
   });
